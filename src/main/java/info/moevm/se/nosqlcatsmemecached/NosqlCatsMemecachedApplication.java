c8ed899a49c354abc38c13f943f37cc8f1adf084
Alexander Terekhov <sasha4888.terehov@gmail.com>
2021-09-14 10:12:45 +0300

    added spring boot web & modifyed groupId

--- src/main/java/info/moevm/se/nosqlcatsmemecached/NosqlCatsMemecachedApplication.java
+++ src/main/java/info/moevm/se/nosqlcatsmemecached/NosqlCatsMemecachedApplication.java
@@ -1,4 +1,4 @@
-package info.moevm.nosqlcatsmemecached;
+package info.moevm.se.nosqlcatsmemecached;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
