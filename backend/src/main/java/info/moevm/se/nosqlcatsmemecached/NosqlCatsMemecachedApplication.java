4f9f3e84b1c0fd5e96dbdc08d0449351794c442b
Alexander Terekhov <sasha4888.terehov@gmail.com>
2021-09-17 14:57:31 +0300

    move files into backend directory & rewrite description

--- backend/src/main/java/info/moevm/se/nosqlcatsmemecached/NosqlCatsMemecachedApplication.java
+++ backend/src/main/java/info/moevm/se/nosqlcatsmemecached/NosqlCatsMemecachedApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NosqlCatsMemecachedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NosqlCatsMemecachedApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NosqlCatsMemecachedApplication.class, args);
+    }
 
 }
