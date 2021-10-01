23edca3830688a33e926bfd6bda0718a0bde9cd8
Nikita Nechepurenko <rufflerock@gmail.com>
2021-10-01 17:39:55 +0300

    refactor getCachedKeys with functional style

--- backend/src/main/java/info/moevm/se/nosqlcatsmemecached/dao/CatsDaoImpl.java
+++ backend/src/main/java/info/moevm/se/nosqlcatsmemecached/dao/CatsDaoImpl.java
@@ -2,11 +2,8 @@ package info.moevm.se.nosqlcatsmemecached.dao;
 
 import info.moevm.se.nosqlcatsmemecached.models.cat.Cat;
 import info.moevm.se.nosqlcatsmemecached.utils.memcached.CatsMemcachedClient;
-import java.net.SocketAddress;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import net.spy.memcached.internal.OperationFuture;
 import org.springframework.context.annotation.Primary;
@@ -52,42 +49,15 @@ public class CatsDaoImpl implements CatsDao {
         return false;
     }
 
-    public List<String> getCachedKeys(){
-        Set<Integer> slabIds = new HashSet<>();
-        Map<SocketAddress, Map<String, String>> stats;
-        List<String> keyNames = new ArrayList<>();
-//        return client.getStats("items").values().stream()
-//              .map(Map::keySet)
-//              .flatMap(Collection::stream)
-//              .map(propertyName -> propertyName.split(":")[1])
-//              .map(slabId -> client.getStats("cachedump " + slabId + " 0"))
-//              .map(Map::values)
-//              .flatMap(Collection::stream)
-//              .map(Map::keySet)
-//              .flatMap(Collection::stream)
-//              .collect(Collectors.toList());
-
-
-        // Gets all the slab IDs
-        stats = client.getStats("items");
-        stats.forEach((socketAddress, value) -> {
-            System.out.println("Socket address: "+socketAddress.toString());
-            value.forEach((propertyName, propertyValue) -> {
-                slabIds.add(Integer.parseInt(propertyName.split(":")[1]));
-            });
-        });
-
-        // Gets all keys in each slab ID and adds in List keyNames
-        slabIds.forEach(slabId -> {
-            Map<SocketAddress, Map<String, String>> keyStats = client.getStats("cachedump "+slabId+" 0");
-            keyStats.forEach((socketAddress, value) -> {
-                value.forEach((propertyName, propertyValue) -> {
-                    keyNames.add(propertyName);
-                });
-            });
-        });
-
-        return keyNames;
+    public List<String> getCachedKeys() {
+        return client.getStats("items").values().stream()
+                     .map(Map::keySet).flatMap(Collection::stream)
+                     .map(propertyName -> propertyName.split(":")[1])
+                     .collect(Collectors.toSet()).stream()
+                     .map(slabId -> String.format("cachedump %s 0", slabId))
+                     .map(client::getStats)
+                     .map(Map::values).flatMap(Collection::stream)
+                     .map(Map::keySet).flatMap(Collection::stream)
+                     .collect(Collectors.toList());
     }
-
 }
