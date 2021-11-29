1b8765dace4474c064c48582bb59712819da1926
p.mironchik <mairon-pasha@yandex.ru>
2021-11-29 10:23:37 +0300

    update cats fields names

--- frontend/src/list/CatModel.js
+++ frontend/src/list/CatModel.js
@@ -1,18 +1,18 @@
 class CatModel {
   constructor(raw, id) {
     this.id = id;
-    this.name = raw['breed_name'];
+    this.name = raw['breedName'];
     this.care = raw['care'];
     this.characteristics = raw['characteristics'];
-    this.children = raw['children_and_pets'];
-    this.grooming = raw['color_and_grooming'];
+    this.children = raw['childrenAndPets'];
+    this.grooming = raw['colorAndGrooming'];
     this.health = raw['health'];
     this.history = raw['history'];
     this.personality = raw['personality'];
-    this.image = raw['round_img_url'];
-    this.short_description = raw['short_description'];
+    this.image = raw['roundImgUrl'];
+    this.short_description = raw['shortDescription'];
     this.size = raw['size'];
-    this.stats = raw['vital_stats'];
+    this.stats = raw['vitalStats'];
   }
 }
 
